86e8a7b33efb64facb36250b173fdbee163cdc66
Sean Baines <mrseanbaines@gmail.com>
2018-09-22 14:05:58 +0100

    Install Chai and Sinon

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import MyComponent from './MyComponent';
 import App from './App';
+
+// import ReactDOM from 'react-dom';
 
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
 //   ReactDOM.render(<App />, div);
 //   ReactDOM.unmountComponentAtNode(div);
 // });
+
+describe('<MyComponent />', () => {
+  it('renders three <App /> components', () => {
+    const wrapper = shallow(<MyComponent />);
+    expect(wrapper.find(App)).to.have.lengthOf(3);
+  });
+
+  it('renders an `.icon-star`', () => {
+    const wrapper = shallow(<MyComponent />);
+    expect(wrapper.find('.icon-star')).to.have.lengthOf(1);
+  });
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <MyComponent>
+        <div className="unique" />
+      </MyComponent>
+    ));
+    expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+  });
+
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = shallow(<App onButtonClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+  });
+});
