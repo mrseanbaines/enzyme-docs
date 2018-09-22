60e826a30023ae8c4bc8b4fc19f28f2c2e6d3d13
Sean Baines <mrseanbaines@gmail.com>
2018-09-22 14:37:59 +0100

    Check that Foo allows us to set props

---
+++ src/full-dom-rendering.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+import Foo from './Foo';
+
+describe('<Foo />', () => {
+  it('allows us to set props', () => {
+    const wrapper = mount(<Foo bar="baz" />);
+    expect(wrapper.props().bar).to.equal('baz');
+    wrapper.setProps({ bar: 'foo' });
+    expect(wrapper.props().bar).to.equal('foo');
+  });
+});
