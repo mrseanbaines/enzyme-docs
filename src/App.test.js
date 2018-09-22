4b0bf65d5dd71f83fcca6fdf5f4a35d73d90af9f
Sean Baines <mrseanbaines@gmail.com>
2018-09-22 14:20:10 +0100

    Check that App renders 3 Foo components

--- src/App.test.js
+++ src/App.test.js
@@ -3,41 +3,12 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
-import MyComponent from './MyComponent';
 import App from './App';
+import Foo from './Foo';
 
-// import ReactDOM from 'react-dom';
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-
-describe('<MyComponent />', () => {
-  it('renders three <App /> components', () => {
-    const wrapper = shallow(<MyComponent />);
-    expect(wrapper.find(App)).to.have.lengthOf(3);
-  });
-
-  it('renders an `.icon-star`', () => {
-    const wrapper = shallow(<MyComponent />);
-    expect(wrapper.find('.icon-star')).to.have.lengthOf(1);
-  });
-
-  it('renders children when passed in', () => {
-    const wrapper = shallow((
-      <MyComponent>
-        <div className="unique" />
-      </MyComponent>
-    ));
-    expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-  });
-
-  it('simulates click events', () => {
-    const onButtonClick = sinon.spy();
-    const wrapper = shallow(<App onButtonClick={onButtonClick} />);
-    wrapper.find('button').simulate('click');
-    expect(onButtonClick).to.have.property('callCount', 1);
+describe('<App />', () => {
+  it('renders three <Foo /> components', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Foo)).to.have.lengthOf(3);
   });
 });
